8b2c9bf941c06cbe6b0f9bda35d1828641921a38
Arijit231998 <arijitbakshi998@gmail.com>
2022-08-15 16:30:08 +0600

    first commit

---
+++ library/models.py
@@ -0,0 +1,29 @@
+from distutils.command.upload import upload
+from typing_extensions import Self
+from unicodedata import category
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime,timedelta
+
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models. PositiveIntegerField()
+    category=models.CharField(max_length=100)
+
+class Student (models.Model):
+        user= models.OneToOneField(User,on_delete=models.CASCADE)
+        classroom= models.CharField(max_length=40)
+        branch=models.CharField(max_length=40)
+        roll_no= models.CharField(max_length=8,blank=True)
+        phone=models.CharField(max_length=11,blank=True)
+        image= models.ImageField(upload_to="", blank=True)
+       
+
+def expire(){
+    return str(self.user)+"[+str(self.branch)+']"
+}
+
+
+
