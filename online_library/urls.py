eb34323e73d7856a405ad3731fd5cba0f7d75215
Arijit231998 <arijitbakshi998@gmail.com>
2022-08-18 10:59:25 +0600

    commited

--- online_library/urls.py
+++ online_library/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 #from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("",views.index, name="index"),
+    path("add_book/",views.add_book,name="add_book"),
+    path("view_books/",views.view_books, name="view_book"),
 ]
