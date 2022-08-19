aab5e078d30aa356527dd82ab1818061abe8a682
Arijit231998 <arijitbakshi998@gmail.com>
2022-08-19 11:30:31 +0600

    commited

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns={
     path("",views.index,name="index"),
-    path("add_books/",views.add_books,name="add_books"),
+    path("add_book/",views.add_books,name="add_books"),
     path("view_students/",views.view_students,name="view_students"),
+    path("issue_book/",views.issue_book,name="issue_book"),
 }
