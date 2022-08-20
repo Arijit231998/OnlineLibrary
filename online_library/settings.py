ce7caf9878e53f22666528935e0bd8821f75230d
Arijit231998 <arijitbakshi998@gmail.com>
2022-08-20 17:07:07 +0600

    commited

--- online_library/settings.py
+++ online_library/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'online_library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["library/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
