e5245541525d0c64c0a2fc0847fef552b02af9f5
Arijit231998 <arijitbakshi998@gmail.com>
2022-08-23 19:23:24 +0600

    commited

--- online_library/settings.py
+++ online_library/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'library/media')
 MEDIA_URL = '/media/'
-
+STATICFILES_DIRS =[
+      os.path.join(BASE_DIR, "library/static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
